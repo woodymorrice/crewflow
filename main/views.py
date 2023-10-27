793e4e917fa55effa7f939c8484a5f1efd21e280
Hongshen Wang (how791) <how791@mail.usask.ca>
2023-10-27 13:41:34 -0600

    pasted missing function to views.py

--- main/views.py
+++ main/views.py
@@ -59,3 +59,69 @@ def create_blog_post(request):
 
     return render(request, './main/addBlog.html', {'form': form})
 
+def employee_payroll(request):
+    """
+    page for view employee profile
+    """
+    employees = [
+        {
+            'name': 'Employee 1',
+            'salary': 5000,
+            'bonus': 2000,
+            'deductions': 1000,
+        },
+        {
+            'name': 'Employee 2',
+            'salary': 6000,
+            'bonus': 2500,
+            'deductions': 1200,
+        },
+        # Add more employees as needed
+    ]
+
+    # Calculate total payroll expenses
+    total_payroll_expenses = sum(employee['salary'] + employee['bonus'] for employee in employees)
+
+    # Calculate total deductions
+    total_deductions = sum(employee['deductions'] for employee in employees)
+
+    # Calculate total net payments
+    total_net_payments = total_payroll_expenses - total_deductions
+
+    # Calculate the average salary
+    total_salaries = sum(employee['salary'] for employee in employees)
+    average_salary = total_salaries / len(employees)
+
+    current_date = date.today()
+
+    # Calculate the start date (1st day of the current month)
+    payroll_start_date = current_date.replace(day=1)
+
+    # Calculate the end date (15th day of the current month)
+    if current_date.day <= 15:
+        payroll_end_date = current_date
+    else:
+        # If today is past the 15th, set the end date to the 15th of the next month
+        next_month = current_date.replace(day=15)  # 15th of the current month
+        payroll_end_date = (next_month.replace(month=next_month.month + 1) if next_month.month < 12
+                            else next_month.replace(year=next_month.year + 1, month=1))
+
+    # Format the dates as strings
+    payroll_period = f"{payroll_start_date.strftime('%B')} {payroll_start_date.day}, {payroll_start_date.year} " \
+                     f"- {payroll_end_date.strftime('%B')} {payroll_end_date.day}, {payroll_end_date.year}"
+
+    context = {
+        'total_payroll_expenses': total_payroll_expenses,
+        'total_deductions': total_deductions,
+        'total_net_payments': total_net_payments,
+        'average_salary': average_salary,
+        'number_of_employees': len(employees),
+        'total_bonus_payments': sum(employee['bonus'] for employee in employees),
+        'payroll_period': payroll_period,
+        'payroll_status': 'Pending Manager Approval',
+        'user_is_manager': True,
+    }
+
+    return render(request, 'main/payroll.html', context)
+
+
