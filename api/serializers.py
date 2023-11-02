89c45eb83512779cde41a934732a4fc4c8f1fff3
Jeffrey <jeffrey.xia@usask.ca>
2023-11-01 23:41:23 -0600

    update

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from main.models import BlogPost
-
-
-class BlogItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlogPost
-        fields = '__all__'
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/blog/list/', views.getData),
-    path('api/blog/add/', views.addPost),
-    path('api/blog/delete-data/<int:id>/', views.deleteData, name='delete-data'),
-]
