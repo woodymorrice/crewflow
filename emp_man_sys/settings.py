9ff7d724caa11cc76e765c53b819d171ee97afc2
Woody Morrice <wam553@usask.ca>
2023-11-27 22:56:11 -0600

    fixed broken logos

--- emp_man_sys/settings.py
+++ emp_man_sys/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_FINDERS = 'djangobower.finders.BowerFinder',
+# STATICFILES_FINDERS = 'djangobower.finders.BowerFinder',
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
